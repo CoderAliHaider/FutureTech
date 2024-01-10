8a9789d0b5cdd4f61dcf42b55c6e39ec1b415923
Ali Haider <alihaiderhameed007@gmail.com>
2024-01-10 20:56:50 +0500

    add images

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,9 +1,15 @@
-export default function Shop() {
+import { Image } from "react-bootstrap";
+
+const  Shop = () => {
   return (
     <main>
       <div className="user_title flex p-4">
         <h1 className="text-4xl font-semibold ">SHOP</h1>
       </div>
+      <div>
+        <div><Image src="/images/doc_duck.png" alt="pic" /></div>
+      </div>
     </main>
   );
 }
+export default Shop
